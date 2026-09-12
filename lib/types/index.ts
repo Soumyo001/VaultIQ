c5a3262fe4fdd4ba113420bd80b11bee41812870
soumyo001 <www.soumyo@gmail.com>
2026-09-12 17:30:02 +0600

    update budget schema

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,1 +1,5 @@
 export * from '@/lib/types/user.type';
+export * from '@/lib/types/account.type';
+export * from '@/lib/types/budget.type';
+export * from '@/lib/types/category.type';
+export * from '@/lib/types/transaction.type';
